7cb8a152fc0088bdb310f5b093c300e68cb4539b
Jean Paul <jeanpaulcora@gmail.com>
2024-05-21 18:47:59 -0400

    añadido de funciones de archivo.h escribir

--- funciones_escribir_archivo.h
+++ funciones_escribir_archivo.h
@@ -5,3 +5,15 @@ void reiniciar_archivo(char nombre_archivo[]);
 
 // escribe los detalles donantes en el archivo
 void escribir_detalle_donaciones(char nombre_archivo[], lista_donantes* list);
+
+// escribir los detalles donates en el archivo
+void escribir_asignacion_nodo_donaciones(char nombre_archivo[], lista_necesidad* lista_necesidad);
+
+// escribe el detalle de articulos
+void escribir_detalle_articulos(char nombre_archivo[], lista_articulos* lista_articulos);
+
+// escribe todos los detalles de los donantes
+void escribir_detalles_donantes(char nombre_archivo[], lista_donantes* lista_donantes);
+
+// escribe toda la informacion en el archivo
+void escribir_archivo(char nombre_archivo[], lista_articulos* list_articulos, lista_donantes* list_donantes, lista_necesidad* list_necesidad);
